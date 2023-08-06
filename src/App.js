a218167ac2677792f82f92ee66ac45963f80eca5
algokekz <krrodri6@asu.edu>
2023-08-06 12:42:48 -0700

    button stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button'
 
 function App() {
   return (
@@ -10,10 +10,31 @@ function App() {
         </p>
         <img src={require("./assets/barbie_poster.png")} className="App-logo" alt="barbie" />
         <img src={require("./assets/oppenheimer_poster.png")} className="App-logo" alt="oppenheimer" />
-      
       </header>
     </div>
   );
 }
+
+const oppenheimerButton = {
+  backgroundColor: "black",
+  color: "white",
+  flexDirection: "row",
+  float: "left",
+  alignItems: "center",
+  width: 100,
+  height: 50,
+  borderRadius: 100
+};
+
+const barbieButton = {
+  backgroundColor: "pink",
+  color: "black",
+  flexDirection: "row",
+  float: "left",
+  alignItems: "center",
+  width: 100,
+  height: 50,
+  borderRadius: 100
+};
 
 export default App;
