e9120b77821a67a00f498c9f50d366254786b456
algokekz <krrodri6@asu.edu>
2023-08-06 17:39:41 -0700

    jesus fucking christ

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
 import { Button } from './components/Button'
+import { Counter } from './components/Counter'
 import React from 'react';
 
+
 function App() {
+  /*setTimeout(() => { 
+    window.location.reload(); 
+  }, 5000);*/
   return (
     <div className="App">
       <header className="App-header">
         <p>
         Click for your favorite movie: Barbie or Oppenheimer!!!
         </p>
-        <div className='App-column'>
+        <div className='App-grida'>
           <img src={require("./assets/barbie_poster.png")} className="App-logo" alt="barbie" />
           <img src={require("./assets/oppenheimer_poster.png")} className="App-logo" alt="oppenheimer" />
         </div>
-        <div className='App-grid'>
-          <Button label = {"Barbie"} style = {barbieButton}/>
-          <Button label = {"Oppenheimer"} style = {oppenheimerButton} />
+        <div className='App-gridb'>
+          <Counter label = {"Barbie"} style = {barbieButton}/>
+          <Counter label = {"Oppenheimer"} style = {oppenheimerButton} />
         </div>
       </header>
     </div>
@@ -25,7 +30,7 @@ function App() {
 const oppenheimerButton = {
   backgroundColor: "black",
   color: "white",
-  width: 100,
+  width: 170,
   height: 50,
   borderRadius: 100
 };
@@ -33,7 +38,7 @@ const oppenheimerButton = {
 const barbieButton = {
   backgroundColor: "pink",
   color: "black",
-  width: 100,
+  width: 150,
   height: 50,
   borderRadius: 100
 };
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export const Button = ({ label, style, handleClick }) => (
+  
+  
   <button
     style={style}
     onClick={handleClick}
